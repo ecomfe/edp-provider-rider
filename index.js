1cbbbebf1c20fd043f7fa56726417753db3d8b7c
Firede <firede@firede.us>
2014-03-05 05:20:57 +0800

    added `options.resolveUrl` && fixed bugs.

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ var rider = require('rider');
 var ap = require('autoprefixer');
 
 /**
- * autoprefixer 
+ * autoprefixer
+ * 
  * @param  {Array}   args      autoprefixer options
  * @param  {Function} callback provider callback
  * @return {Function}          autoprefixer function
@@ -24,38 +25,39 @@ function prefixer(args, callback) {
             this, 
             args || [ "android >= 2.3", "ios >= 5", "ie >= 10" ]
             ).process(css).css;
-
     };
     
 }
 
 /**
  * 编译
+ * 
  * @param  {Object}     options  options
  * @param  {boolean=}   options.implicit  引入rider
- * @param  {Array=}     options.autoprefixer  autoprefixer支持
+ * @param  {Array|boolean=}     options.autoprefixer  autoprefixer支持
+ * @param  {boolean=}   options.resolveUrl resolve url
  * @param  {Function=}  options.use  use
  * @param  {Function=}  callback provider callback
  */
 function plugin(options, callback) { 
 
-    return function (stylus) {
+    return function (style) {
 
-        stylus.use(
+        style.use(
             rider({
                 implicit: options.implicit
             })
         )
-        .on('end', prefixer(options.autoprefixer, callback));
+        .set('resolve url', options.resolveUrl || true);
 
-        if (options.use) {
-
-            stylus.use(options.use);
-
+        if (options.autoprefixer !== false) {
+            style.on('end', prefixer(options.autoprefixer, callback));
         }
 
+        if (options.use) {
+            style.use(options.use);
+        }
     };
-
 }
 
 module.exports = exports = {
